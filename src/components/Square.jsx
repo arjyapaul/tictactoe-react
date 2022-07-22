bf351df5bbf694db63c00ad91d902085fefca71a
Arjya Paul <arjyapaul14@gmail.com>
2022-07-22 13:32:59 +0530

    sass styling

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const square = ({value}) => {
   return (
-    <button>{value}</button>
+    <button className='square'>{value}</button>
   )
 }
 
