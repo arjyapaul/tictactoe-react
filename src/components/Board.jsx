3a605f187f2b39cfcc391dbcc2b1062472e2668d
Arjya Paul <arjyapaul14@gmail.com>
2022-07-28 12:59:25 +0530

    winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,24 +1,8 @@
-import React,{useState} from 'react'
+import React from 'react'
 import square from './Square';
 import Square from './Square'
 
-const board = () => {
-  const [board,setBoard]=useState(Array(9).fill(null));
-  const [isXNext,setIsXNext]=useState(false);
-  const handleSquareClick=position=>{
-    if(board[position]){
-      return;
-    }
-    setBoard((prev)=>{
-      return prev.map((square,pos)=>{
-        if(pos===position){
-          return isXNext?'X':'O';
-        }
-        return square;
-      });
-    });
-    setIsXNext((prev)=>!prev);
-  };
+const board = ({board,handleSquareClick}) => {
   const renderSquare=position=>{
     return(
       <Square
